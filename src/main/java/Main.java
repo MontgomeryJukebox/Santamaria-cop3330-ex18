68c78709f6d8a012ee5c9267dd9e04c9cc4452bf
David Santamaria <davidsantamaria1245@gmail.com>
2021-06-04 00:11:04 -0400

    Upload files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 /*
  *  UCF COP3330 Summer 2021 Assignment 1 Solution
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 David Santamaria
  */
 public class Main {
     public static void main(String[] args) {
